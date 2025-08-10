eba0ebbb00012e5abfaa92d4fbc0002851f79ef5
calishu <danny.kretschmann@proton.me>
2025-08-10 23:11:00 +0200

    fix: added `$` at beginning of `{required_version}`

--- format.sh
+++ format.sh
@@ -4,7 +4,7 @@ set -e
 required_version=19
 current_version=$(clang-format --version | grep -o '[0-9]\+' | head -1)
 
-if [ "${current_version}" -lt "{required_version}" ]; then
+if [ "${current_version}" -lt "${required_version}" ]; then
 	echo "[error] clang-format version $required_version+ is required. Your current version is ${current_version}."
 	exit 1
 fi
