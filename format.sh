89b9fd015067db8dabf465e1913ea7f406877c7c
calishu <danny.kretschmann@proton.me>
2025-08-10 23:34:03 +0200

    feat(`format.sh`): add check if `clang-format` is installed

--- format.sh
+++ format.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
 
-required_version=19
-current_version=$(clang-format --version | grep -o '[0-9]\+' | head -1)
-
-if [ "${current_version}" -lt "${required_version}" ]; then
-	echo "[error] clang-format version $required_version+ is required. Your current version is ${current_version}."
+if ! command -v "clang-format" >/dev/null 2>&1; then
+	echo "clang-format isn't installed on your system. Abort."
 	exit 1
 fi
 
-find src include -name '*.cpp' -o -name '*.hpp' | xargs clang-format -i
+required_version=19
+current_version=$(clang-format --version | grep -o "[0-9]\+" | head -1)
+
+if [ "${current_version}" -lt "${required_version}" ]; then
+	echo "[error] clang-format version $required_version+ is required, but your current version is ${current_version}."
+	exit 1
+fi
+
+find src include -name "*.cpp" -o -name "*.hpp" | xargs clang-format -i
