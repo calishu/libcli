d879454bac34b109041090865ef6295ba1d09e8c
calishu <danny.kretschmann@proton.me>
2025-08-11 19:28:03 +0200

    feat(`options.hpp`): added `OptionObject` and `is_parsed` bool in `FlagObject`

--- src/libcli/options.hpp
+++ src/libcli/options.hpp
@@ -9,10 +9,28 @@ struct FlagObject {
     std::string_view description_;
     void (*cb)();
 
+    bool is_parsed = false;
+
     constexpr FlagObject(std::string_view name, std::string_view description, void (*callback)() = nullptr)
         : name_(name),
           description_(description),
           cb(callback) {};
+};
+
+struct OptionObject {
+    std::string_view name_;
+    std::string_view description_;
+    void (*cb)();
+
+    bool is_parsed = false;
+    bool expected_ = false;
+
+    constexpr OptionObject(std::string_view name, std::string_view description, void (*callback)() = nullptr)
+        : name_(name),
+          description_(description),
+          cb(callback) {};
+
+    constexpr auto expected(bool state = true) -> void { expected_ = state; }
 };
 
 }; // namespace LibCLI
