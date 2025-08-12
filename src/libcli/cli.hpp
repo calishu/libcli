c93998ac2bb5d6aa03d6dec141bff6e0e10cb79e
calishu <danny.kretschmann@proton.me>
2025-08-12 03:31:11 +0200

    feat: add master-header

---
+++ src/libcli/cli.hpp
@@ -0,0 +1,17 @@
+/* Copyright (c) 2025 calishu
+ * This piece of software is licensed under the `Apache 2.0` license,
+ * for more details about it, check the `LICENSE` file or visit:
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * GitHub Repository: https://github.com/calishu/libcli
+ * Documentation: https://github.com/calishu/libcli/blob/main/docs
+ * Ko-Fi: https://ko-fi.com/calishu
+ *
+ * If you include this file in your project you will import everything,
+ * I would recommend you to import only the headers you need.
+ * */
+
+#pragma once
+
+#include <libcli/app.hpp>
+#include <libcli/options.hpp>
